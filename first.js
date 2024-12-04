80bf5f5af2c9317b8f90f0f346ba2c0774dfdd57
Shaarim24 <shaarimalam888@gmail.com>
2024-12-04 07:56:08 -0800

    Add files via upload

--- first.js
+++ first.js
@@ -1,12 +1,11 @@
-fullName = "tony star"
-age = 24
-price = 99.99
-radius = 14
+let fullName = "tony star"
+const age = 24
+let price = 99.99
+var radius = 14
 a = null;
 y = undefined;
 isFollow = true;
 
 $fullname = 42468274
-fullName = 25
 
 console.log(fullName)
