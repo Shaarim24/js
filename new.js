8c36665c6101f5faf374a460196f44e0d12fc5dc
Shaarim24 <shaarimalam888@gmail.com>
2024-12-25 09:09:36 -0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,15 @@
+{
+    let a = 5;
+    console.log(a);
+}
+{
+    let a =10;
+    console.log(a);
+}
+//types:number,string,boolean,undefined,null,bigint,symbol
+const student = {
+    fullName: "Rahul Kumar",
+    age: 20,
+    cgpa: 8.2,
+    isPass: true
+};
